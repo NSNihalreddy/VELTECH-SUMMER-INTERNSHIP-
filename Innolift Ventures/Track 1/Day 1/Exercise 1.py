f43e1c4d14d7d7b6b91e269fd018d6a9b58a5923
NSNihalreddy <vtu28683@veltech.edu.in>
2026-06-01 11:23:13 +0530

    Track 1 Day 1 Exercise 1

---
+++ Innolift Ventures/Track 1/Day 1/Exercise 1.py
@@ -0,0 +1,6 @@
+name='NAREDDY SHANMUKHA NIHALREDDY'
+age=19
+college='VELTECH UNIVERSITY'
+branch='CSE(AI&DS)'
+CGPA=8.63
+print(f'My name is {name}.I am {age} studying {branch} at {college} with {CGPA} CGPA')
