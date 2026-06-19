a929cadfc06ec899d6a4bcb10be9e69b2b62d993
NSNihalreddy <vtu28683@veltech.edu.in>
2026-06-19 08:28:47 +0530

    Update app.py

--- Innolift Ventures/Customer-Churn-Prediction-System/app.py
+++ Innolift Ventures/Customer-Churn-Prediction-System/app.py
@@ -81,7 +81,7 @@ def register():
 
             flash("Email already registered. Login with existing account.")
             return redirect("/register")
-            return render_template("register.html")
+    return render_template("register.html")
 @app.route("/logout")
 def logout():
     session.clear()
